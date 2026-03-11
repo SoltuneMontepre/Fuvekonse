862fbce617ed81bb4df9901421aa16a365cdcdd6
Makiato <iceblueberriest@gmail.com>
2026-03-11 16:21:18 +0700

    fix: maximum 5 urls

--- services/general-service/internal/dto/dealer/requests/dealer_register_request.go
+++ services/general-service/internal/dto/dealer/requests/dealer_register_request.go
@@ -3,5 +3,5 @@ package requests
 type DealerRegisterRequest struct {
 	BoothName   string   `json:"booth_name" binding:"required,min=1,max=255"`
 	Description string   `json:"description" binding:"required,min=1,max=500"`
-	PriceSheets []string `json:"price_sheets" binding:"required,min=1,dive,required,url,max=500"`
+	PriceSheets []string `json:"price_sheets" binding:"required,min=1,max=5,dive,required,url,max=500"`
 }
